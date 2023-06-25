7bc212ee77654b343c4e1d44aec88eea171bab0a
Ilmanzi <ilman.aziss@gmail.com>
2023-06-25 18:35:48 +0700

    Adding experience

--- controllers/experienceController.js
+++ controllers/experienceController.js
@@ -1,4 +1,24 @@
 class ExperienceController {
+    static async getAll(req, res, next) {
+
+    }
+
+    static async getById(req, res, next) {
+        
+    }
+
+    static async create(req, res, next) {
+
+    }
+
+    static async update(req, res, next) {
+
+    }
+
+    static async delete(req, res, next) {
+
+    }
+        
 }
 
 module.exports = ExperienceController
