95ed8cd3def2053df761d7b874aac1f7afc49fbd
dila <nfebriani@eskalink.com>
2023-06-27 08:14:35 +0700

    feat(controller): create get method for user profile

--- controllers/userprofileController.js
+++ controllers/userprofileController.js
@@ -1,4 +1,35 @@
+const { UserProfile } = require('../models');
+
 class UserProfileController {
+    
+    static async getAllUserProfile(req, res, next) {
+        try {
+            const userprofile = await UserProfile.findAll()
+            res.status(200).json(userprofile)
+        } catch (err) {
+            next(err)
+        }
+    };
+
+    static async getUserProfileById(req, res, next) {
+        try {
+            const user_id = req.params.user_id;
+
+            const userprofile = await UserProfile.findOne({
+                where: {
+                    user_id
+                }
+            });
+
+            if (!userprofile) {
+                throw { name: "ErrorNotFound" }
+            }
+            
+            res.status(200).json(userprofile)
+        } catch (err) {
+            next(err)
+        }
+    }
 }
 
 module.exports = UserProfileController
