323f28e6a8c98e534b08b03794d918ee2069b976
almaduri <ali.mdrw@gmail.com>
2023-06-30 03:52:14 +0700

    add getAllTypes, getTypeById, createType, updateType, deleteType and routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const authRouter = require("./auth");
 const userRouter = require("./user");
 const jobListingRouter = require("./joblisting");
 const companyProfileRouter = require("./companyprofile")
+const typeRouter = require("./type");
 const { authentication, authorization } = require("../middlewares/auth");
 const UserController = require("../controllers/userController")
 
@@ -14,5 +15,6 @@ router.use("/api/job_listing", jobListingRouter);
 // router.use(authorization(["admin", "user", "recruiter"])); // BELOW HERE AUTHORIZATION ROUTES
 router.use("/api/users", userRouter);
 router.use("/api/companyProfile", companyProfileRouter);
+router.use("/api/types", typeRouter);
 
 module.exports = router;
