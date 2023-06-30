9d30ee1568fec8b24ba16ee9628a6e6e51dd632f
ferdi <dinaldese@gmail.com>
2023-06-30 23:43:03 +0700

    educations

--- migrations/20230630142025-create-user-education.js
+++ migrations/20230630142025-create-user-education.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("UserEducation", {
+    await queryInterface.createTable("UserEducations", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -44,6 +44,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("UserEducation");
+    await queryInterface.dropTable("UserEducations");
   },
 };
