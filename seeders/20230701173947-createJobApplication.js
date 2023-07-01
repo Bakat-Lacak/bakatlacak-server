4e81412b6d5cadb038271a517fbb090fb94fab24
Ilmanzi <ilman.aziss@gmail.com>
2023-07-02 00:51:29 +0700

    adding JobApplication seeder

---
+++ seeders/20230701173947-createJobApplication.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const { User, JobListing } = require("../models")
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    const jobList = await JobListing.findOne({where: {
+      title: "Full-stack Web Developer"
+    }})
+
+    const userAccount = await User.findOne({where: {
+      email: "user@mail.com"
+    }})
+
+    await queryInterface.bulkInsert('JobApplications', [
+      {
+        user_id: userAccount.id,
+        joblisting_id: jobList.id,
+        status: 'applied',
+        resume: "link:resume",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
