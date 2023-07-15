165e53f28dcfc05e749954073cd15f7976b0f13a
Ilmanzi <ilman.aziss@gmail.com>
2023-07-15 16:50:59 +0700

    fixing routes by user id

--- routes/experience.js
+++ routes/experience.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const ExperienceController = require("../controllers/experienceController");
 
 router.get("/", ExperienceController.getAll);
-router.get("/:id", ExperienceController.getById);
 router.get("/me", ExperienceController.getByUserId);
+router.get("/:id", ExperienceController.getById);
 router.post("/", ExperienceController.create);
 router.put("/:id", ExperienceController.update);
 router.delete("/:id", ExperienceController.delete);
