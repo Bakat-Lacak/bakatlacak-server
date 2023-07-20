d69175a0e021ec93c1a7669eade16683bfc773b1
danielzuhad <danielzuhad@gmail.com>
2023-07-20 22:08:10 +0700

    not done yet with getAllJObApplyByUserID

--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,13 @@ const skillRouter = require("./skill");
 const jobApplicationRouter = require("./jobapplication");
 const { authentication, authorization } = require("../middlewares/auth");
 
+router.use("/api/job_application", jobApplicationRouter);
 router.use("/api/auth", authRouter);
 router.use(authentication); // BELOW HERE AUTHENTICATED ROUTES
 router.use("/api/users", userRouter);
 router.use("/api/experiences", experienceRouter);
 router.use("/api/user-profiles", userprofileRouter);
 router.use("/api/job_listing", jobListingRouter);
-router.use("/api/job_application", jobApplicationRouter);
 
 // router.use(authorization(["admin", "user", "recruiter"])); // BELOW HERE AUTHORIZATION ROUTES
 router.use("/api/users", userRouter);
